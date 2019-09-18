c68c5103e445e379ed6ae3b5ec0dbd2a28188ab5
Antoine Raquillet <antoine.raquillet@esiea.fr>
2019-09-18 09:03:17 +0200

    Added ITI.UserManagement.

---
+++ ITI.UserManagement/ITI.UserManagement.UI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ITI.UserManagement.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User john = new User();
+            john.SetPassword("ERTERTER");
+            if (john.PasswordMatch("spidof"))
+            {
+                Console.WriteLine("Success!");
+            }
+            else
+            {
+                Console.WriteLine("Failure!");
+            }
+        }
+    }
+}
---
+++ ITI.UserManagement/ITI.UserManagement.UI/User.cs
@@ -0,0 +1,24 @@
+﻿namespace ITI.UserManagement.UI
+{
+    class User
+    {
+        string _userName;
+        string _password;
+
+        public string UserName
+        {
+            get { return _userName; }
+            set { _userName = value; }
+        }
+
+        public void SetPassword(string password)
+        {
+            _password = password;
+        }
+
+        public bool PasswordMatch(string candidate)
+        {
+            return _password == candidate;
+        }
+    }
+}
