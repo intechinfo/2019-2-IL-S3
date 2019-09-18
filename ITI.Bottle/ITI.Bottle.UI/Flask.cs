85415d0eb217d08e71b6afc94e07361df49a37be
Antoine Raquillet <antoine.raquillet@esiea.fr>
2019-09-18 10:42:00 +0200

    Added ITI.Bottle.

---
+++ ITI.Bottle/ITI.Bottle.UI/Flask.cs
@@ -0,0 +1,18 @@
+﻿namespace ITI.Bottle.UI
+{
+    class Flask
+    {
+        ushort _maxCapacity;
+        ushort _currentVolume;
+
+        public Flask()
+            : this(1000)
+        {
+        }
+
+        public Flask(ushort maxCapacity)
+        {
+            _maxCapacity = maxCapacity;
+        }
+    }
+}
---
+++ ITI.Bottle/ITI.Bottle.UI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ITI.Bottle.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Flask flask1 = new Flask(1500);
+            Flask flask2 = new Flask();
+        }
+    }
+}
