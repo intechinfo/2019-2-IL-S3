0481c998bfc7435c6387b90b66a71169fd9b611c
Antoine Raquillet <antoine.raquillet@esiea.fr>
2019-09-23 10:02:03 +0200

    Added ITI.BankAccount.

---
+++ ITI.BankAccount/ITI.BankAccount/Account.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ITI.BankAccount
+{
+    class Account
+    {
+        readonly string _number;
+
+        int _balance;
+        string _password;
+
+        public Account(string number, string password, int balance)
+        {
+            _number = number != null && number.Length >= 8 ? number : Guid.NewGuid().ToString();
+            _password = password != null && password.Length == 4 ? password : Guid.NewGuid().ToString().Substring(0, 4);
+            _balance = Math.Max(balance, 0);
+        }
+
+        public void Credit(int amount)
+        {
+            _balance += Math.Max(amount, 0);
+        }
+
+        public void Debit(int amount, string password)
+        {
+            if (amount < 0 || amount > _balance) return;
+            if (password != _password) return;
+
+            _balance -= amount;
+        }
+
+        public int GetBalance(string password)
+        {
+            return password != _password ? -1 : _balance;
+        }
+
+        public void SetPassword(string oldPassword, string newPassword)
+        {
+            if (oldPassword != _password) return;
+            if (newPassword == null) return;
+            if (newPassword.Length != 4) return;
+
+            _password = newPassword;
+        }
+    }
+}
---
+++ ITI.BankAccount/ITI.BankAccount/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ITI.BankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account("FR3712-1234-5678-9012-123", "1234", 0);
+
+            account.Credit(-3000);
+            account.Credit(1000);
+
+            account.Debit(500, "0000");
+            account.Debit(-3000, "1234");
+            account.Debit(2000, "1234");
+            account.Debit(-3000, "0000");
+            account.Debit(200, "1234");
+
+            Console.WriteLine(account.GetBalance("0000"));
+            Console.WriteLine(account.GetBalance("1234"));
+
+            account.SetPassword("0000", "6543");
+            Console.WriteLine(account.GetBalance("6543"));
+
+            account.SetPassword("1234", "654321");
+            Console.WriteLine(account.GetBalance("654321"));
+
+            account.SetPassword("1234", "4321");
+            Console.WriteLine(account.GetBalance("4321"));
+            Console.WriteLine(account.GetBalance("1234"));
+        }
+    }
+}
