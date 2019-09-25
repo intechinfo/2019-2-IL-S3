612906c2174be417d62ea89350d419151980de3a
Antoine Raquillet <antoine.raquillet@esiea.fr>
2019-09-25 14:47:31 +0200

    Added ITI.Geometry.

---
+++ ITI.Geometry/ITI.Geometry/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ITI.Geometry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Segment s = new Segment(3, 5);
+            DisplaySegment(s);
+
+            s.Left = 2;
+            DisplaySegment(s);
+
+            s.Right = 10;
+            DisplaySegment(s);
+
+            s.Move(2);
+            DisplaySegment(s);
+
+            s.Move(-4);
+            DisplaySegment(s);
+
+            Console.WriteLine("Is 0 on [3, 8[ ? {0}", s.IsOn(0));
+            Console.WriteLine("Is 4 on [3, 8[ ? {0}", s.IsOn(4));
+            Console.WriteLine("Is 12 on [3, 8[ ? {0}", s.IsOn(12));
+
+            s.Left = 0;
+            Console.WriteLine("[0, 5[ overlaps [0, 5[ ? {0} - expected: True", s.IsOverlaping(Segment.Create(0, 5)));
+            Console.WriteLine("[0, 5[ overlaps [-1, 6[ ? {0} - expected: True", s.IsOverlaping(Segment.Create(-1, 6)));
+            Console.WriteLine("[0, 5[ overlaps [-2, -1[ ? {0} - expected: False", s.IsOverlaping(Segment.Create(-2, -1)));
+            Console.WriteLine("[0, 5[ overlaps [6, 8[ ? {0} - expected: False", s.IsOverlaping(Segment.Create(6, 8)));
+            Console.WriteLine("[0, 5[ overlaps [3, 4[ ? {0} - expected: True", s.IsOverlaping(Segment.Create(3, 4)));
+            Console.WriteLine("[0, 5[ overlaps [-2, 3[ ? {0} - expected: True", s.IsOverlaping(Segment.Create(-2, 3)));
+            Console.WriteLine("[0, 5[ overlaps [3, 8[ ? {0} - expected: True", s.IsOverlaping(Segment.Create(3, 8)));
+
+            DisplaySegment(s.GetIntersection(Segment.Create(-2, 3)));
+            DisplaySegment(s.GetIntersection(Segment.Create(3, 4)));
+            DisplaySegment(s.GetIntersection(Segment.Create(6, 8)));
+
+            Console.WriteLine("[0, 5[ is equal to [0, 5[ ? {0} - expected: true", s.IsEqual(Segment.Create(0, 5)));
+            Console.WriteLine("[0, 5[ is equal to [1, 4[ ? {0} - expected: false", s.IsEqual(Segment.Create(1, 4)));
+            Console.WriteLine("[0, 5[ is equal to [0, 5[ ? {0} - expected: true", s.IsEqual(s));
+        }
+
+        static void DisplaySegment(Segment s)
+        {
+            if (s == null) Console.WriteLine("The segment is null.");
+            else Console.WriteLine("Left: {0}, right: {1}, length; {2}", s.Left, s.Right, s.Length);
+        }
+    }
+}
---
+++ ITI.Geometry/ITI.Geometry/Segment.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITI.Geometry
+{
+    class Segment
+    {
+        readonly int _length;
+        int _position;
+
+        public Segment(int position, int length)
+        {
+            _position = position;
+            _length = Math.Abs(length);
+        }
+
+        public static Segment Create(int point1, int point2)
+        {
+            int left = Math.Min(point1, point2);
+            int right = Math.Max(point1, point2);
+            return new Segment(left, right - left);
+        }
+
+        public void Move(int delta)
+        {
+            _position += delta;
+        }
+
+        public int Left
+        {
+            get { return _position; }
+            set { _position = value; }
+        }
+
+        public int Right
+        {
+            get { return _position + _length; }
+            set { _position = value - _length; }
+        }
+
+        public int Length
+        {
+            get { return _length; }
+        }
+
+        public bool IsOn(int point)
+        {
+            return _position <= point && point < Right;
+        }
+
+        public bool IsOverlaping(Segment other)
+        {
+            return IsOn(other.Left) || IsOn(other.Right) || other.IsOn(Left);
+        }
+
+        public Segment GetIntersection(Segment other)
+        {
+            if (!IsOverlaping(other)) return null;
+
+            return Segment.Create(
+                Math.Max(Left, other.Left),
+                Math.Min(Right, other.Right));
+        }
+
+        public static Segment GetIntersection(Segment s1, Segment s2)
+        {
+            return s1.GetIntersection(s2);
+        }
+
+        public bool IsEqual(Segment segment)
+        {
+            return _position == segment._position && _length == segment._length;
+        }
+    }
+}
