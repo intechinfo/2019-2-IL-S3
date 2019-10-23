894615a2c7ab4da4bfdbe8553ef8b2959315aaf1
Antoine Raquillet <antoine.raquillet@esiea.fr>
2019-10-23 10:12:02 +0200

    Added Windows Forms demo.

---
+++ WindowFormsDemo/WindowFormsDemo/MainForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowFormsDemo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.okButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(12, 17);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(242, 26);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(261, 13);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(108, 35);
+            this.okButton.TabIndex = 1;
+            this.okButton.Text = "O.k.";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_onClicked);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(12, 58);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(0, 20);
+            this.outputLabel.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 119);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.inputTextBox);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ WindowFormsDemo/WindowFormsDemo/MainForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowFormsDemo
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        void okButton_onClicked(object sender, EventArgs e)
+        {
+            outputLabel.Text = inputTextBox.Text;
+        }
+    }
+}
