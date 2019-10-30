54fc2dd6d91b24fb765350fd377fba28907a5871
Antoine Raquillet <antoine.raquillet@esiea.fr>
2019-10-30 09:53:30 +0100

    Added ITI.SFMLExample.Model.

---
+++ ITI.SFMLExample/ITI.SFMLExample.Model/GameContext.cs
@@ -0,0 +1,17 @@
+﻿namespace ITI.SFMLExample.Model
+{
+    public class GameContext
+    {
+        readonly Player _player;
+
+        public GameContext()
+        {
+            _player = new Player(this);
+        }
+
+        public Player Player
+        {
+            get { return _player; }
+        }
+    }
+}
---
+++ ITI.SFMLExample/ITI.SFMLExample.Model/Player.cs
@@ -0,0 +1,39 @@
+﻿namespace ITI.SFMLExample.Model
+{
+    public class Player
+    {
+        readonly GameContext _ctx;
+        double _energy;
+        double _x;
+        double _y;
+
+        internal Player(GameContext ctx)
+        {
+            _ctx = ctx;
+            _x = 0;
+            _y = 0;
+            _energy = 1;
+        }
+
+        public double Energy
+        {
+            get { return _energy; }
+        }
+
+        public double X
+        {
+            get { return _x; }
+        }
+
+        public double Y
+        {
+            get { return _y; }
+        }
+
+        public void Move(double deltaX, double deltaY)
+        {
+            _x += deltaX;
+            _y += deltaY;
+        }
+    }
+}
